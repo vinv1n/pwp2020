f977acb7fe814e1a75cd06f77825dce89f367c32
Juho Sipola <jusipola@student.oulu.fi>
2020-03-16 11:40:09 +0200

    Add database models

--- backend/api/database/models.py
+++ backend/api/database/models.py
@@ -1,4 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
 
 from api import db
 
@@ -14,6 +15,10 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     observations = db.relationship("Observation", back_populates="user")
 
+    # relationships
+    devices = relationship("Device")
+    device_groups = relationship("DeviceGroup")
+
     @staticmethod
     def create_password(password):
         return generate_password_hash(password, method="sha512")
@@ -23,6 +28,10 @@ class User(db.Model):
 
 
 class Observation(db.Model):
+    """
+    Observations made by users
+
+    """
 
     __tablename__ = "observations"
     id = db.Column(db.Integer, primary_key=True)
@@ -40,3 +49,84 @@ class Observation(db.Model):
     # connect observations to a user
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user = db.relationship(User, back_populates="observations")
+
+    rating = db.relationship("Rating")
+    location_id = db.Column(db.Interger, db.ForeignKey("location.id"))
+
+
+class UserAlert(db.Model):
+    """
+    Alerts user when certain weather conditions are met
+    """
+    __tablename__ = "alerts"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    humidity = db.Column(db.Float, nullable=True)
+    temperature = db.Column(db.Float, nullable=True)
+    pressure = db.Column(db.Float, nullable=True)
+    wind = db.Column(db.Float, nullable=True)
+    condition = db.Column(db.Enum(WeatherTypes))
+
+
+class Location(db.Model):
+    """
+    Geographic location of the observation.
+
+    """
+    __tablename__ = "locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(150))
+    longitude = db.Column(db.String(75))
+    latitude = db.Column(db.String(75))
+
+    observations = db.relationship("Observation")
+
+
+
+class Rating(db.Model):
+    """
+    User rating of observations.
+
+    """
+    __tablename__ = "ratings"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    observation_id = db.Column(db.Integer, db.ForeignKey("observation.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    content = db.Column(db.String)
+    value = db.Column(db.Interger)
+
+
+
+class Device(db.Model):
+    """
+    Devices registered by user which measure weather information
+    and sends them to application
+
+    """
+    __tablename__ = "devices"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+
+    location_id = db.Column(db.Intreger, db.ForeignKey("location.id"))
+    user_id = db.Column(db.Interger, db.ForeignKey("user.id"))
+    groups = relationship("DeviceGroup", back_populates="devices")
+
+
+class DeviceGroup(db.Model):
+    """
+    Groups of devices created by user
+    """
+    __tablename__ = "groups"
+
+    id = db.Column(db.Interger, primary_key=True)
+    user_id = db.Column(db.Interger, db.ForeignKey("user.id"))
+
+    # list of devices that are member of this group
+    members = relationship("Device", back_populates="groups")
