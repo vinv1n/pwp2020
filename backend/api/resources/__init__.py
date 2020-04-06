eaabdad3346ae73e74506c5456611a04630c0dd7
Juho Sipola <jusipola@student.oulu.fi>
2020-04-06 11:25:32 +0300

    Export all resources

--- backend/api/resources/__init__.py
+++ backend/api/resources/__init__.py
@@ -1,8 +1,32 @@
 from .upload import Upload
 from .login import LoginApi
+from .user import UserItem, UserCollection
+from .rate import RateItem, RateCollection
+from .alert import AlertCollection, AlertItem
+from .device import DeviceCollection, DeviceItem
+from .group import GroupCollection, GroupItem, UsersGroupCollection
+from .location import LocationCollection, LocationItem
+from .observation import ObservationCollection, ObservationItem
+from .upload import Upload
 
 
 __all__ = [
     "Upload",
-    "LoginApi"
+    "LoginApi",
+    "AlertCollection",
+    "AlertItem",
+    "DeviceItem",
+    "DeviceCollection",
+    "GroupItem",
+    "GroupCollection",
+    "UsersGroupCollection",
+    "LocationItem",
+    "LocationCollection",
+    "ObservationItem",
+    "ObservationCollection",
+    "Upload",
+    "UserItem",
+    "UserCollection",
+    "RateItem",
+    "RateCollection"
 ]
