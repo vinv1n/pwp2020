0b1cee5dc7c4a3996bc79dd50bd8f53957951c5d
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2020-04-06 18:48:22 +0300

    Start work on device resource

--- backend/api/resources/device.py
+++ backend/api/resources/device.py
@@ -1,11 +1,38 @@
 from flask_restful import Resource
+from flask import request, Response, jsonify, abort
+from api.database.models import Device
 
 
 class DeviceCollection(Resource):
 
-    def post(self):
-        pass
+    def get(self, User):
+        """ Get all devices """
+        # TODO the returned devices should have proper META such as controls with them
+        answer = Device.query.filter_by().all()
+        return jsonify(answer)
 
+    def post(self, User):
+        if not request.json:
+            abort(415)
+        try:
+            dev = Device(
+                name=request.json["template"]["name"],
+                latitude=request.json["template"]["latitude"],
+                longitude=request.json["template"]["latitude"],
+                user_id = User
+            )
+            db.session.add(dev)
+            db.session.commit()
+            # Get the location of the newly created device
+            location_string = {
+                "Location": api.url_for(DeviceItem, handle=request.json["template"]["name"])
+                }
+            return Response(headers=location_string, status=201, mimetype='application/json')
+        except KeyError:
+            abort(400)
+        except:
+            abort(500)    
+        pass
 
 class DeviceItem(Resource):
 
@@ -13,6 +40,9 @@ class DeviceItem(Resource):
         pass
 
     def put(self, device):
+        if not request.json:
+            abort(415)
+            
         pass
 
     def delete(self, device):
