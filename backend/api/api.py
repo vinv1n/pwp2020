35b53d2cff8e8f0991f83ca9cf1087d34f40c99f
Risto Salminen <ripejcp@gmail.com>
2020-04-29 13:48:36 +0300

    Cleanup and give database to the resources

--- backend/api/api.py
+++ backend/api/api.py
@@ -9,21 +9,19 @@ from .resources import (
     LoginApi,
     ObservationCollection,
     ObservationItem,
-    Upload,
     UserCollection,
     UserItem,
     UsersGroupCollection,
 )
 
-api.add_resource(EntryPoint, "/api/")
-api.add_resource(DeviceCollection, "/api/devices/")
-api.add_resource(DeviceItem, "/api/devices/<device>/")
-api.add_resource(GroupCollection, "/api/groups/")
-api.add_resource(GroupItem, "/api/groups/<group>/")
+api.add_resource(EntryPoint, "/api/", resource_class_kwargs={'db': db})
+api.add_resource(DeviceCollection, "/api/devices/", resource_class_kwargs={'db': db})
+api.add_resource(DeviceItem, "/api/devices/<device>/", resource_class_kwargs={'db': db})
+api.add_resource(GroupCollection, "/api/groups/", resource_class_kwargs={'db': db})
+api.add_resource(GroupItem, "/api/groups/<group>/", resource_class_kwargs={'db': db})
 api.add_resource(LoginApi, "/api/auth/login", resource_class_kwargs={"db": db})
-api.add_resource(ObservationCollection, "/api/observations/")
-api.add_resource(ObservationItem, "/api/observations/<observation>/")
-api.add_resource(Upload, "/api/upload", resource_class_kwargs={'db': db})
-api.add_resource(UserCollection, "/api/users/")
-api.add_resource(UserItem, "/api/users/<user>/")
-api.add_resource(UsersGroupCollection, "/api/users/<user>/groups/")
+api.add_resource(ObservationCollection, "/api/observations/", resource_class_kwargs={'db': db})
+api.add_resource(ObservationItem, "/api/observations/<observation>/", resource_class_kwargs={'db': db})
+api.add_resource(UserCollection, "/api/users/", resource_class_kwargs={'db': db})
+api.add_resource(UserItem, "/api/users/<user>/", resource_class_kwargs={'db': db})
+api.add_resource(UsersGroupCollection, "/api/users/<user>/groups/", resource_class_kwargs={'db': db})
