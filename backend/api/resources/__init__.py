3124a24ad372a5302198624ba30f72db03924d49
Risto Salminen <ripejcp@gmail.com>
2020-04-29 13:49:29 +0300

    Cleanup and add missing resource initialisations

--- backend/api/resources/__init__.py
+++ backend/api/resources/__init__.py
@@ -1,32 +1,21 @@
-from .upload import Upload
 from .login import LoginApi
 from .user import UserItem, UserCollection
-from .rate import RateItem, RateCollection
-from .alert import AlertCollection, AlertItem
 from .device import DeviceCollection, DeviceItem
 from .group import GroupCollection, GroupItem, UsersGroupCollection
-from .location import LocationCollection, LocationItem
 from .observation import ObservationCollection, ObservationItem
-from .upload import Upload
+from .entry import EntryPoint
 
 
 __all__ = [
-    "Upload",
     "LoginApi",
-    "AlertCollection",
-    "AlertItem",
     "DeviceItem",
     "DeviceCollection",
+    "EntryPoint",
     "GroupItem",
     "GroupCollection",
     "UsersGroupCollection",
-    "LocationItem",
-    "LocationCollection",
     "ObservationItem",
     "ObservationCollection",
-    "Upload",
     "UserItem",
     "UserCollection",
-    "RateItem",
-    "RateCollection"
 ]
