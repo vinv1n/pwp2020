b4f10f1d0cba16d191005ddd42a650033a6fdc4a
Risto Salminen <ripejcp@gmail.com>
2020-04-29 14:10:51 +0300

    Improve client a bit

--- client/static/scripts/client.js
+++ client/static/scripts/client.js
@@ -9,10 +9,8 @@
 const COLLECTIONJSON = "application/vnd.collection+json";
 const PLAINJSON = "application/json";
 
-// Directly borrowed from the PWP exercise 4.
-function renderError(jqxhr) {
-    let msg = jqxhr.responseJSON["collection"]["error"]["message"];
-    $("div.notification").html("<p class='error'>" + msg + "</p>");
+function appendObservationRow(body) {
+    $(".resulttable tbody").append(observationRow(body));
 }
 
 // Directly borrowed from the PWP exercise 4.
@@ -22,6 +20,13 @@ function getResource(href, renderer) {
         success: renderer,
         error: renderError,
     });
+}
+
+function getSubmittedObservation(data, status, jqxhr) {
+    let href = jqxhr.getResponseHeader("Location");
+    if (href) {
+        getResource(href, appendObservationRow);
+    }
 }
 
 function observationRow(item) {
@@ -36,6 +41,12 @@ function observationRow(item) {
         + "<td>" + data["location"] + "</td>"
         + "<td>" + data["observed-at"] + "</td>"
         + "</tr>";
+}
+
+// Directly borrowed from the PWP exercise 4.
+function renderError(jqxhr) {
+    let msg = jqxhr.responseJSON["collection"]["error"]["message"];
+    $("div.notification").html("<p class='error'>" + msg + "</p>");
 }
 
 function renderObservation(body) {
@@ -53,25 +64,29 @@ function renderObservationForm(data, href, method) {
     form.attr("action", href);
     form.attr("method", method);
     form.submit(submitObservation);
-    new_data = {}
+    let new_data = {};
     data.forEach(function (item) {
-        new_data[item.name] = item;
+        new_data[item.name.replace("-", "_")] = item;
     });
-    order = [
+    console.log(new_data);
+    let order = [
         "location",
-        "observed-at",
+        "observed_at",
         "temperature",
         "wind",
-        "wind-direction",
+        "wind_direction",
         "humidity",
     ];
     order.forEach(function (item) {
-        form.append("<label>" + new_data[item].prompt + "</label>");
-        form.append(
-            "<input type='text' "
-            + "name='" + new_data[item].name + "' "
-            + "value='" + new_data[item].value + "'>"
-        );
+        console.log(new_data[item]);
+        if (item in new_data) {
+            form.append("<label>" + new_data[item].prompt + "</label>");
+            form.append(
+                "<input type='text' "
+                + "name='" + new_data[item].name + "' "
+                + "value='" + new_data[item].value + "'>"
+            );
+        }
     });
     form.append("<input type='submit' name='submit' value='Submit'>");
     $("div.form").html(form);
@@ -82,12 +97,16 @@ function renderObservations(body) {
     $(".resulttable thead").html(
         "<tr><th>Location</th><th>Observed at</th></tr>"
     );
-    let tbody = (".resulttable tbody");
+    let tbody = $(".resulttable tbody");
     tbody.empty();
     body.collection.items.forEach(function (item) {
         tbody.append(observationRow(item));
     });
-    renderObservationForm(body.collection);
+    renderObservationForm(
+        body.collection.template.data,
+        body.collection.href,
+        "POST"
+    );
 }
 
 // Directly borrowed from the PWP exercise 4.
@@ -101,6 +120,18 @@ function sendData(href, method, item, postProcessor) {
         success: postProcessor,
         error: renderError,
     });
+}
+
+function submitObservation(event) {
+    event.preventDefault();
+
+    let data = {};
+    let form = $("div.form form");
+    console.log($("div.form form input"));
+    $.makeArray($("div.form form input")).forEach(function (item) {
+        data[item.name] = item.value;
+    });
+    sendData(form.attr("action"), form.attr("method"), data, getSubmittedObservation);
 }
 
 $(document).ready(function () {
