5f09ea1b70f98054f07faf91afd8400f50aa4ab2
Risto Salminen <ripejcp@gmail.com>
2020-04-29 14:06:56 +0300

    Use CORS to be able to use a client

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, abort, redirect
+from flask_cors import CORS
 from flask_restful import Api
 
 from .database import WeatherTalkDatabase
 from .config import Config
 
 app = Flask(__name__)
+CORS(app)
 
 # load flask configurations
 config = Config()
