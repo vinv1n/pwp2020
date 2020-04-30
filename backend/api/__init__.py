37e6d7c6af58a45de19ae0cf9d1e77d434de0325
Risto Salminen <ripejcp@gmail.com>
2020-04-30 14:21:52 +0300

    Expose the Location header for CORS

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -5,8 +5,13 @@ from flask_restful import Api
 from .database import WeatherTalkDatabase
 from .config import Config
 
+expose_headers = [
+    "Location",
+]
+
+
 app = Flask(__name__)
-CORS(app)
+CORS(app, expose_headers=expose_headers)
 
 # load flask configurations
 config = Config()
