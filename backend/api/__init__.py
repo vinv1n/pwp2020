da0cf4600244da14555c048e303133ec89fc4039
Risto Salminen <ripejcp@gmail.com>
2020-04-30 10:33:23 +0300

    Define the Collection+JSON mediatype in one place

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -5,6 +5,10 @@ from flask_restful import Api
 from .database import WeatherTalkDatabase
 from .config import Config
 
+
+COLLECTIONJSON = "application/vnd.collection+json"
+
+
 app = Flask(__name__)
 CORS(app)
 
