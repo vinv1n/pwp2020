77b793b93cf9e2455e69153e64fdfc2bdadbbda9
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2020-05-05 15:55:27 +0300

    Create UsersGroupItem endpoint
    This endpoint is to have the '/api/users/{user_id}/groups/{group_id}/ URL.
    A device group should not exist without a user

--- backend/api/resources/group.py
+++ backend/api/resources/group.py
@@ -1,9 +1,27 @@
 from flask_restful import Resource
+import logging
 
+from flask_restful import Resource
+from flask import json, request, Response, jsonify, abort, url_for
+from sqlalchemy.exc import IntegrityError
+
+from api.database import DeviceGroup
+
+from .. import COLLECTIONJSON, api
+
+from .utils import (
+    CollectionJsonBuilder,
+    CollectionJsonItemBuilder,
+    create_error_response,
+    create_500_error,
+)
+from .resource_maps import device_group
+
+logger = logging.getLogger(__name__)
 
 class GroupCollection(Resource):
 
-    def post(self):
+    def post(self, user):
         pass
 
 
@@ -20,6 +38,49 @@ class GroupItem(Resource):
 
 
 class UsersGroupCollection(Resource):
+    def post(self, user):
+        device_group = DeviceGroup()
+        body = request.get_json(force=True)
+        try:
+            data = body["template"]["data"]
+        except KeyError:
+            return self.create_400_error()
+
+        for item in data:
+            name = item.get("name")
+            value = item.get("value")
+            if not all((name, value)):
+                return self.create_400_error(
+                    "{} is a required field".format(name)
+                )
+            if "-" in name:
+                name = name.replace("-", "_")
+            setattr(device_group, name, value)
+
+        self.database.session.add(device_group)
+        try:
+            self.database.session.commit()
+        except IntegrityError:
+            self.database.session.rollback()
+            return self.create_400_error()
+        headers = {
+            "Location": api.url_for(UsersGroupItem,
+                                    user=user,
+                                    group=device_group.id),
+        }
+        return Response(status=201, headers=headers)
 
     def get(self, user):
+        pass
+
+
+class UsersGroupItem(Resource):
+
+    def get(self, user, group):
+        pass
+
+    def put(self, user, group):
+        pass
+
+    def delete(self, user, group):
         pass
