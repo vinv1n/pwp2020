188b467112e432951967a92ddb5ad7a13041aa80
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2020-05-06 20:32:48 +0300

    Remove login api endpoint

--- backend/api/resources/__init__.py
+++ backend/api/resources/__init__.py
@@ -1,4 +1,3 @@
-from .login import LoginApi
 from .user import UserItem, UserCollection
 from .device import DeviceCollection, DeviceItem
 from .group import GroupCollection, GroupItem, UsersGroupCollection, UsersGroupItem
